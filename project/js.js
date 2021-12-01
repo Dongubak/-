2d8c63c0a32b8f4cb12cdd495996aa58bf3cd53e
Dongubak <91736442+Dongubak@users.noreply.github.com>
2021-12-01 23:35:36 +0900

    lasted 수정

--- project/js.js
+++ project/js.js
@@ -1,75 +1,78 @@
 function init() {
-    const $btn = document.querySelector("#input__btn");
+    const $btn = document.querySelector("#container__btn");
     $btn.addEventListener("click", function(event) {
         event.preventDefault();
         secinit();
-
-
     });
 }
-
 function secinit() {
-    const $input_2 = document.querySelector("#input__2").value;
-    const $input_3 = document.querySelector("#input__3").value;
-    const $input_4 = document.querySelector("#input__4").value;
-    console.log($input_4);
+    const container_2 = document.querySelector("#container__2").value;
+    const container_4 = document.querySelector("#container__4").value;
     console.log("secinit");
-    ///원금은 필수 임
-    ///1.이율을 입력하지않은경우
-    ///2.횟수를 입력하지 않은 경우
-    ///3.목표금액을 입력하지 않은 경우
-    if($input_4 == "") {
+    if(container_4 == "") {
         nonInput4();
     } else {
-        if($input_2 == "") {
+        if(container_2 == "") {
             nonInput2();
+        } else {
+            nonInput3();
         }
-
     }
-
-    /*
-    if($input_2.value == undefined) {
-        nonInput2();
-    }
-    */
-
 }
-
-
-
-function nonInput4() {
-    const $input_1 = parseInt(document.querySelector("#input__1").value);
-    const $input_2 = parseFloat(document.querySelector("#input__2").value);
-    const $input_3 = parseInt(document.querySelector("#input__3").value);
-    console.log("nonInput4");
-    const $input_list = document.querySelector("#input__list");
-    const result = $input_1*(1+$input_2)**$input_3;
-    const $input_text = document.createTextNode(result);
-    $input_list.appendChild($input_text);
-}
-
-
 function nonInput2() {  
-    const $input_1 = parseInt(document.querySelector("#input__1").value);
-    const $input_3 = parseInt(document.querySelector("#input__3").value);
-    const $input_4 = parseInt(document.querySelector("#input__4").value);
+    const container_1 = parseInt(document.querySelector("#container__1").value);
+    const container_3 = parseInt(document.querySelector("#container__3").value);
+    const container_4 = parseInt(document.querySelector("#container__4").value);
     console.log("nonInput2");
+    const $container__ul = document.querySelector("#container2__ul");
     let i = 1;
     let result;
+    
     while(true) {
-        result = $input_1*(1+i*0.001)**$input_3;
+        const $Cli = document.createElement("li");
+        const $Ctext = document.createTextNode('');
+        result = container_1*(1+i*0.001)**container_3;
         if(i === 1000) {
             alert("이율이 100%이상입니다.");
             break;
         }
 
-        if($input_4 < result) {
+        if(container_4 < result) {
+            $Ctext.textContent = result;
+            $Cli.appendChild($Ctext);
+            $container__ul.appendChild($Cli);
+            break;
+        }
+        i++;
+        $Ctext.textContent = result;
+        $Cli.appendChild($Ctext);
+        $container__ul.appendChild($Cli);
+    }
+}
+function nonInput3() {
+    const container_1 = parseInt(document.querySelector("#container__1").value);
+    const container_2 = parseFloat(document.querySelector("#container__2").value);
+    const container_4 = parseInt(document.querySelector("#container__4").value);
+    console.log("nonInput3");
+    let i =1;
+    let result;
+    while(true) {
+        result = container_1*(1+container_2)**i;
+        if(container_4 < result) {
+            console.log(result);
+            console.log("넘는 순간의", i);
             break;
         }
         i++;
         console.log(result);
     }
 }
-
-
+function nonInput4() {
+    const container_1 = parseInt(document.querySelector("#container__1").value);
+    const container_2 = parseFloat(document.querySelector("#container__2").value);
+    const container_3 = parseInt(document.querySelector("#container__3").value);
+    console.log("nonInput4");
+    const result = container_1*(1+container_2)**container_3;
+    console.log(result);
+}
 init();
